286f87dc46c54905bf471530279a9974cc3e82d8
Adán Alvarez <geroshima123@gmail.com>
2024-02-26 04:18:13 -0300

    Add new color variables

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,11 +5,16 @@ import Link from "next/link";
 
 const Header = ({ titleButton, signIn = true, titleLink }) => {
   return (
+<<<<<<< HEAD
     <header className="flex flex-col items-center justify-evenly pt-2 md:flex-row lg:flex-row lg:items-center lg:pt-0 ">
       <div className="order-last flex flex-row items-center justify-around md:order-1 lg:order-1">
+=======
+    <header className="flex flex-col lg:flex-row md:flex-row items-center lg:items-center md:items-center justify-evenly pt-2">
+      <div className="flex flex-row items-center justify-around order-last lg:order-1 md:order-1">
+>>>>>>> d0d95fc (Add layout and page components)
         <Link
           href="#"
-          className="py-1 pr-8 text-lg font-medium text-gray-800 no-underline hover:text-gray-900"
+          className="py-1 pr-8 text-lg font-medium text-text-color no-underline hover:text-gray-900"
           style={{ borderRight: "1px solid #BFBFBF", display: "inline-block" }}
         >
           Discover
@@ -17,7 +22,7 @@ const Header = ({ titleButton, signIn = true, titleLink }) => {
 
         <Link
           href="#"
-          className="ml-8 flex items-center text-lg font-medium text-gray-800 no-underline hover:text-gray-900"
+          className="ml-8 flex items-center text-lg font-medium text-text-color no-underline hover:text-gray-900"
         >
           <FontAwesomeIcon icon={faPlus} className="mr-4 h-5 w-5  text-lg" />
           Start a New Project
@@ -41,6 +46,7 @@ const Header = ({ titleButton, signIn = true, titleLink }) => {
         </Link>
       </div>
 
+<<<<<<< HEAD
       <div className="order-2 flex flex-row items-center justify-between md:order-3 lg:order-3">
         {signIn && (
           <>
@@ -60,6 +66,24 @@ const Header = ({ titleButton, signIn = true, titleLink }) => {
         )}
 
         <button className="h-10 w-40 rounded-lg border-none bg-[#07C051] px-4 py-2 text-lg font-bold text-white hover:cursor-pointer active:bg-[#33ad64]">
+=======
+      <div className="flex flex-row items-center justify-between order-2 lg:order-3 md:order-3">
+        {signIn ? (
+          <Link
+            href="#"
+            className="mr-11 flex items-center text-lg font-medium text-text-color no-underline hover:text-gray-900"
+          >
+            <FontAwesomeIcon
+              icon={faSearch}
+              className="mr-9 h-5 w-5 text-lg"
+            />
+            {titleLink}
+          </Link>
+        ) : (
+          ""
+        )}
+        <button className="h-10 w-40 rounded-lg text-white bg-primary-green border-none bg-green px-4 py-2 text-lg hover:cursor-pointer active:bg-secondary-green">
+>>>>>>> d0d95fc (Add layout and page components)
           {titleButton}
         </button>
       </div>
