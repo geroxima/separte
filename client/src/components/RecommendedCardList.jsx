1a282dffd14d28f5da07bfd19afd50695010568b
Maylén Zaracho <142035725+maylenzaracho@users.noreply.github.com>
2024-02-28 22:27:37 -0300

    Update RecommendedCardList.jsx

--- client/src/components/RecommendedCardList.jsx
+++ client/src/components/RecommendedCardList.jsx
@@ -3,11 +3,11 @@ import RecommendedCard from "./RecommendedCard";
 
 const data = [
   {
-    title: "Campaign 1",
-    fundraiserName: "Fundraiser 1",
+    title: "Campaña 1",
+    fundraiserName: "Recaudación de fondos 1",
     fundraiserImg:
       "https://img.freepik.com/premium-vector/avatar-icon002_750950-52.jpg?size=338&ext=jpg&ga=GA1.1.1700460183.1708300800&semt=sph",
-    fundraiserLocation: "Location 1",
+    fundraiserLocation: "Ubicación 1",
     description: "lorem ipsum dolor sit amet",
     currentAmount: 1000,
     targetAmount: 5000,
@@ -15,24 +15,24 @@ const data = [
     backers: 50,
   },
   {
-    title: "Campaign 2",
-    fundraiserName: "Fundraiser 2",
+    title: "Campaña 2",
+    fundraiserName: "Recaudación de fondos 2",
     fundraiserImg:
       "https://img.freepik.com/premium-vector/avatar-icon002_750950-52.jpg?size=338&ext=jpg&ga=GA1.1.1700460183.1708300800&semt=sph",
-    fundraiserLocation: "Location 2",
-    description: "Description 2",
+    fundraiserLocation: "Ubicación 2",
+    description: "lorem ipsum dolor sit amet",
     currentAmount: 2000,
     targetAmount: 6000,
     daysLeft: 20,
     backers: 100,
   },
   {
-    title: "Campaign 2",
-    fundraiserName: "Fundraiser 2",
+    title: "Campaña 3",
+    fundraiserName: "Recaudación de fondos 3",
     fundraiserImg:
       "https://img.freepik.com/premium-vector/avatar-icon002_750950-52.jpg?size=338&ext=jpg&ga=GA1.1.1700460183.1708300800&semt=sph",
-    fundraiserLocation: "Location 2",
-    description: "Description 2",
+    fundraiserLocation: "Ubicación 3",
+    description: "lorem ipsum dolor sit amet",
     currentAmount: 5500,
     targetAmount: 6000,
     daysLeft: 20,
@@ -42,7 +42,7 @@ const data = [
 const RecommendedCardList = () => {
   return (
     <div className="my-20">
-      <div className="text-3xl font-bold">Proyectos recomendados</div>
+      <div className="text-3xl font-bold">Proyectos Recomendados</div>
       <div className="my-4 flex w-full flex-col flex-wrap gap-4 md:flex-row">
         {data.map((campaign) => {
           return <RecommendedCard campaign={campaign} key={campaign.title} />;
