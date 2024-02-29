7e3f9efa3a04ed888d428480958c308cfcbc7627
marcosferr <ferreiramarcosadrian@gmail.com>
2024-02-29 09:47:17 -0300

    Update header links in Header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -53,18 +53,19 @@ const Header = ({ titleButton, titleLink }) => {
           </button>
         ) : (
           <Link
-            href="login"
+            href="/login"
             className="mr-11 flex items-center text-lg font-medium text-text-color no-underline hover:text-gray-900"
           >
             <FontAwesomeIcon icon={faSearch} className="mr-9 h-5 w-5 text-lg" />
             {titleLink}
           </Link>
         )}
-        <button 
-        href="/about"
-        className="bg-green h-10 w-40 rounded-lg border-none bg-primary-green px-4 py-2 text-lg text-white hover:cursor-pointer active:bg-secondary-green">
+        <a
+          href="/about"
+          className="bg-green h-10 w-40 rounded-lg border-none bg-primary-green px-4 py-2 text-center text-lg text-white hover:cursor-pointer active:bg-secondary-green"
+        >
           {titleButton}
-        </button>
+        </a>
       </div>
     </header>
   );
